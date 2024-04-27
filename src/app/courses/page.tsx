129a4ed2b472ebf0bb2785c964d0f8e61dcfc4b7
Vaibhav Ghugretkar <127537492+Vaibhavghugretkar@users.noreply.github.com>
2024-04-27 22:29:43 +0530

    fixed error

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
     <div className='text-white min-h-screen bg-black py-12 pt-36' >
       <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>  
       <div className="flex flex-wrap justify-center">
-            {courseData.courses.map((course) => (
+            {courseData.courses.map((course:any) => (
                 <CardContainer className="inter-var m-4">
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                   <CardItem
@@ -20,6 +20,7 @@ export default function page() {
                   <CardItem
                     as="p"
                     translateZ="60"
+                
                     className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                   >
                     {course.description}
